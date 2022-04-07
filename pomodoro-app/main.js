b66cf28fd950eb79f29b56cfd36f41d6db8cb9a7
Juliancio Carvalho <juliancio_alves@hotmail.com>
2022-04-07 00:11:43 -0300

    feat: timer pomodoro

--- pomodoro-app/main.js
+++ pomodoro-app/main.js
@@ -1,53 +1,34 @@
 const btnStartPomodoroEl = document.querySelector(".start-pomodoro")
+const secondsTextEl = document.querySelector(".seconds")
 
 const circle = document.querySelector(".progress-ring__circle");
 const radius = circle.r.baseVal.value;
 const circumference = radius * 2 * Math.PI;
 
-let mins, perc, initial, totalSecs, seconds;
+let mins, perc, initial, totalSecs, seconds, secondsToShow;
 
 circle.style.strokeDasharray = circumference;
 circle.style.strokeDashoffset = circumference;
 
 function setProgress(percent) {
-    const offset = circumference - (percent / 100) * circumference;
-    circle.style.strokeDashoffset = offset;
+  const offset = circumference - (percent / 100) * circumference;
+  circle.style.strokeDashoffset = offset;
 }
 
 function decrementTime() {
-    if (circle.classList.contains("danger")) {
-      circle.classList.remove("danger");
-    }
-  
-    if (seconds > 0) {
-      perc = Math.ceil(((totalSecs - seconds) / totalSecs) * 100);
-      setProgress(perc);
-      seconds--;
-      initial = window.setTimeout("decrementTime()", 1000);
-      if (seconds < 10) {
-        circle.classList.add("danger");
-      }
-    } else {
-      mins = 0;
-      seconds = 0;
-      bell.play();
-      let btn = localStorage.getItem("btn");
-  
-      if (btn === "focus") {
-        startBtn.textContent = "start break";
-        startBtn.classList.add("break");
-        localStorage.setItem("btn", "break");
-      } else {
-        startBtn.classList.remove("break");
-        startBtn.textContent = "start focus";
-        localStorage.setItem("btn", "focus");
-      }
-      startBtn.style.transform = "scale(1)";
-    }
+  secondsToShow--
+  if (secondsToShow === 0) {
+    secondsToShow = 60
+  }
+  secondsTextEl.innerHTML = secondsToShow < 10 ? `0${secondsToShow}` : secondsToShow
+  perc = Math.ceil(((60 - seconds) / 60) * 100);
+  setProgress(perc);
+  seconds--;
+  window.setTimeout("decrementTime()", 1000);
 }
 
 btnStartPomodoroEl.addEventListener("click", () => {
-    seconds = 1 * 60;
-    totalSecs = 1 * 60;
-    setTimeout(decrementTime, 60)
+  seconds = 60;
+  secondsToShow = 60
+  setTimeout(decrementTime, 60)
 })
