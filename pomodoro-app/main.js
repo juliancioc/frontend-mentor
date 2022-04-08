9707d5647f8a4a8af4c7912997d4924bd7430706
Juliancio Carvalho <juliancio_alves@hotmail.com>
2022-04-07 21:38:47 -0300

    feat: show minutes pomodoro

--- pomodoro-app/main.js
+++ pomodoro-app/main.js
@@ -1,11 +1,13 @@
 const btnStartPomodoroEl = document.querySelector(".start-pomodoro")
 const secondsTextEl = document.querySelector(".seconds")
+const minutesTextEl = document.querySelector(".minutes")
 
 const circle = document.querySelector(".progress-ring__circle");
 const radius = circle.r.baseVal.value;
 const circumference = radius * 2 * Math.PI;
 
-let mins, perc, initial, totalSecs, seconds, secondsToShow;
+let perc, initial, totalSecs, seconds, secondsToShow;
+let mins = 24
 
 circle.style.strokeDasharray = circumference;
 circle.style.strokeDashoffset = circumference;
@@ -15,20 +17,29 @@ function setProgress(percent) {
   circle.style.strokeDashoffset = offset;
 }
 
+function decrementMinutes() {
+  mins--
+  minutesTextEl.innerHTML = mins
+}
+
 function decrementTime() {
-  secondsToShow--
+  secondsTextEl.innerHTML = secondsToShow < 10 ? `0${secondsToShow}` : secondsToShow
+  perc = Math.ceil(((60 - seconds) / 60) * 100)
+
   if (secondsToShow === 0) {
     secondsToShow = 60
+    decrementMinutes()
   }
-  secondsTextEl.innerHTML = secondsToShow < 10 ? `0${secondsToShow}` : secondsToShow
-  perc = Math.ceil(((60 - seconds) / 60) * 100);
-  setProgress(perc);
-  seconds--;
+
   window.setTimeout("decrementTime()", 1000);
+  setProgress(perc)
+  seconds--
+  secondsToShow--
 }
 
 btnStartPomodoroEl.addEventListener("click", () => {
-  seconds = 60;
-  secondsToShow = 60
+  minutesTextEl.innerHTML = 24
+  seconds = 59;
+  secondsToShow = 59
   setTimeout(decrementTime, 60)
 })
