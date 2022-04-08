4eae63ab19e0c1083e0d39f4a735bc8c4e56d36c
Juliancio Carvalho <juliancio_alves@hotmail.com>
2022-04-07 23:45:05 -0300

    feat: show minutes pomodoro

--- pomodoro-app/main.js
+++ pomodoro-app/main.js
@@ -1,4 +1,7 @@
+const startEl = document.querySelector(".time-control-pomodoro")
 const btnStartPomodoroEl = document.querySelector(".start-pomodoro")
+const btnStartShortBreakEl = document.querySelector(".start-short-break")
+const btnStartLongBreakEl = document.querySelector(".start-long-break")
 const secondsTextEl = document.querySelector(".seconds")
 const minutesTextEl = document.querySelector(".minutes")
 
@@ -19,10 +22,14 @@ function setProgress(percent) {
 
 function decrementMinutes() {
   mins--
-  minutesTextEl.innerHTML = mins
+  minutesTextEl.innerHTML = mins;
 }
 
 function decrementTime() {
+  if(mins === 0 && secondsToShow === 0) {
+    secondsTextEl.innerHTM = '0';
+    minutesTextEl.innerHTML = 25
+  }
   secondsTextEl.innerHTML = secondsToShow < 10 ? `0${secondsToShow}` : secondsToShow
   perc = Math.ceil(((60 - seconds) / 60) * 100)
 
@@ -37,9 +44,36 @@ function decrementTime() {
   secondsToShow--
 }
 
-btnStartPomodoroEl.addEventListener("click", () => {
-  minutesTextEl.innerHTML = 24
+startEl.addEventListener("click", () => {
+  circle.style.transition = '0.5s'
+  minutesTextEl.innerHTML = mins
   seconds = 59;
   secondsToShow = 59
   setTimeout(decrementTime, 60)
 })
+
+function handleRemoveBackgroundTabs(element) {
+  element.classList.remove("active")
+}
+
+btnStartPomodoroEl.addEventListener("click", () => {
+  handleRemoveBackgroundTabs(btnStartShortBreakEl)
+  handleRemoveBackgroundTabs(btnStartLongBreakEl)
+
+  btnStartPomodoroEl.classList.add('active')
+})
+
+btnStartShortBreakEl.addEventListener("click", () => {
+  handleRemoveBackgroundTabs(btnStartPomodoroEl)
+  handleRemoveBackgroundTabs(btnStartLongBreakEl)
+
+  btnStartShortBreakEl.classList.add("active")
+})
+
+btnStartLongBreakEl.addEventListener("click", () => {
+  handleRemoveBackgroundTabs(btnStartPomodoroEl)
+  handleRemoveBackgroundTabs(btnStartShortBreakEl)
+
+  btnStartLongBreakEl.classList.add("active")
+})
+
